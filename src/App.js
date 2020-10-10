0a47b6b291cd77d5afc0fc7a60ed4d06cb0af3d0
caffetito <caffetito@gmail.com>
2020-10-10 19:55:23 +0200

    first proto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
 import React from 'react';
 import logo from './logo.svg';
+import 'ui-neumorphism/dist/index.css';
 import './App.css';
+
+import {MdOpacity} from 'react-icons/md'
+import {GiWool, GiCottonFlower, GiFeather, GiEmptyWoodBucketHandle, GiTransparentTubes,GiTShirt} from 'react-icons/gi'
+
+import { Button, IconButton, Icon, ListItem, ListItemGroup, DocCard, Card, CardAction, CardContent, ProgressCircular, Subtitle1, Subtitle2, Fab } from 'ui-neumorphism';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{width: "300px", margin: "0 auto", }}>
+
+
+<ProgressCircular elevated size={200} value={76} width={20} color='#299ae6'style={{margin: "3rem auto 1rem", }}>52</ProgressCircular>
+
+
+<Fab className='button' color='#299ae6'>
+  &nbsp;<span style={{ fontSize: '24px' }}>  <GiEmptyWoodBucketHandle />
+</span>&nbsp;Ruční&nbsp;
+</Fab>
+
+<Fab className='button' color='#299ae6'>
+  &nbsp;<span style={{ fontSize: '24px' }}> <GiWool /> </span>&nbsp;Vlna&nbsp;
+</Fab>
+
+<Fab className='button' color='#299ae6'>
+  &nbsp;<span style={{ fontSize: '24px' }}> <GiCottonFlower /> </span>&nbsp;Bavlna&nbsp;
+</Fab>
+
+<Fab className='button' color='#299ae6'>
+  &nbsp;<span style={{ fontSize: '24px' }}> <GiFeather /></span>&nbsp;Jemné&nbsp;
+</Fab>
+
+<Fab className='button' color='#299ae6'>
+  &nbsp;<span style={{ fontSize: '24px' }}><GiTransparentTubes /></span>&nbsp;Syntetika&nbsp;
+</Fab>
+
+<Fab className='button' color='#299ae6'>
+  &nbsp;<span style={{ fontSize: '24px' }}><GiTShirt /></span>&nbsp;Běžné&nbsp;
+</Fab>
+
+
+  {/* <IconButton className='button' rounded size='large' text={false} color='var(--error)'>
+  <MdOpacity />
+</IconButton>
+
+
+  <IconButton className='button' rounded size='large' text={false} color='var(--error)'>
+  <MdOpacity />
+</IconButton>
+
+
+  <IconButton className='button' rounded size='large' text={false} color='var(--error)'>
+  <MdOpacity />
+</IconButton>
+
+
+  <IconButton className='button' rounded size='large' text={false} color='var(--error)'>
+  <MdOpacity />
+</IconButton>
+
+
+  <IconButton className='button' rounded size='large' text={false} color='var(--error)'>
+  <MdOpacity />
+</IconButton>   */}
+      
     </div>
   );
 }
